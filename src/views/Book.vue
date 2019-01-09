8ed808115da1947b1edcc3453821be3af087cc1a
NikulinIlya <ilia.nikulin@gmail.com>
2019-01-09 22:43:29 +0300

    feat: set book single page, make book query

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="book">
-        <h1>This is a Book page</h1>
+        <ApolloQuery :query="require('@/graphql/queries/Book.gql')" :variables="{ id: $route.params.id }">
+            <template slot-scope="{ result: { data, loading }, isLoading }">
+                <div v-if="isLoading">Loading...</div>
+                <div v-else>
+                    <div>{{ data.book.title }}</div>
+                    <div>{{ data.book.author }}</div>
+                    <img :src="`http://lar-gql-books/img/${data.book.image}`" alt="book cover">
+                </div>
+            </template>
+        </ApolloQuery>
     </div>
 </template>
