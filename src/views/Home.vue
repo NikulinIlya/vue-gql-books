b1ff3f8938e134f0d1abd5ce2b934ef90741a0ba
NikulinIlya <ilia.nikulin@gmail.com>
2019-01-09 19:57:59 +0300

    feat: make single category query, import queries

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="home">
-    <ApolloQuery :query="require('@/graphql/queries/Categories.gql')">
+    <ApolloQuery :query="categoriesQuery">
       <template slot-scope="{ result: { data, loading }, isLoading }">
         <div v-if="isLoading">Loading...</div>
         <div v-else>
-          <a href="#" v-for="category of data.categories" :key="category.id" class="link-margin">
+          <a href="#" class="link-margin" @click.prevent="selectCategory('all')">All</a>
+          <a href="#" class="link-margin" @click.prevent="selectCategory('featured')">Featured</a>
+          <a href="#" v-for="category of data.categories" :key="category.id" @click.prevent="selectCategory(category.id)" class="link-margin">
             {{ category.id }}. {{ category.name }}
           </a>
         </div>
       </template>
     </ApolloQuery>
-    <ApolloQuery :query="require('@/graphql/queries/Books.gql')">
+    <!--<ApolloQuery :query="require('@/graphql/queries/Books.gql')">
       <template slot-scope="{ result: { data, loading }, isLoading }">
         <div v-if="isLoading">Loading...</div>
         <div v-else>
           <div v-for="book of data.books" :key="book.id">
+            {{ book.id }}. {{ book.title }}
+          </div>
+        </div>
+      </template>
+    </ApolloQuery>-->
+
+    <ApolloQuery :query="categoryQuery" :variables="{ id: selectedCategory }">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <div v-for="book of data.category.books" :key="book.id">
             {{ book.id }}. {{ book.title }}
           </div>
         </div>
@@ -24,8 +37,10 @@
 </template>
 
 <script>
-
-import gql from 'graphql-tag'
+// import gql from 'graphql-tag'
+import categoryQuery from '@/graphql/queries/Category.gql'
+import categoriesQuery from '@/graphql/queries/Categories.gql'
+import booksQuery from '@/graphql/queries/Books.gql'
 
 export default {
   name: 'home',
@@ -33,17 +48,27 @@ export default {
   },
   data() {
     return {
+      categoryQuery,
+      categoriesQuery,
+      booksQuery,
+      selectedCategory: 1,
+      query: booksQuery,
       categories: []
     }
   },
-  apollo: {
-    categories: gql`{
-      categories {
-        id
-        name
+  methods: {
+    selectCategory(category) {
+      if (category === 'all') {
+        this.query = booksQuery
+      } else if (category === 'featured') {
+        this.query = booksFeaturedQuery
+      } else {
+        this.query = categoryQuery
       }
-    }`,
-  },
+
+      this.selectedCategory = category
+    }
+  }
 }
 </script>
 
