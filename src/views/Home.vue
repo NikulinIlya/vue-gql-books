0c4c6adebce8a7d847ed8f8d54622fbf4ed3bddc
NikulinIlya <ilia.nikulin@gmail.com>
2019-01-09 19:05:12 +0300

    feat: make categories apollo query

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,45 @@
 <template>
   <div class="home">
-
+    <ApolloQuery :query="require('@/graphql/queries/Categories.gql')">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <a href="#" v-for="category of data.categories" :key="category.id" class="link-margin">
+            {{ category.id }}. {{ category.name }}
+          </a>
+        </div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
 <script>
 
+import gql from 'graphql-tag'
+
 export default {
   name: 'home',
   components: {
-  }
+  },
+  data() {
+    return {
+      categories: []
+    }
+  },
+  apollo: {
+    categories: gql`{
+      categories {
+        id
+        name
+      }
+    }`,
+  },
 }
 </script>
+
+<style>
+  .link-margin {
+    margin-right: 24px;
+
+  }
+</style>
