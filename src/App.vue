2d8fa974d9f2fb76648dcdf1bd5a068d903074e5
NikulinIlya <ilia.nikulin@gmail.com>
2019-01-14 22:56:54 +0300

    feat(front): set navigation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="app" class="font-sans antialiased text-black">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav" class="bg-purple-dark">
+      <div class="container flex items-center justify-between py-6">
+        <div>
+          <a href="/"><img src="./assets/logo.svg" alt=""></a>
+        </div>
+        <div>
+          <router-link to="/" class="text-white hover:text-grey-light">Home</router-link> |
+          <router-link to="/about" class="text-white hover:text-grey-light pl-6">About</router-link>
+        </div>
+      </div>
     </div>
     <router-view/>
   </div>
